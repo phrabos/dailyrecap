99a702eb0869355b17fa43fa6cb64b0cf924384f
Patrick Hrabos <phrabos@gmail.com>
2020-11-17 17:50:14 -0500

    input fields now populate table, added a non-functioning delete button to row

--- script.js
+++ script.js
@@ -1,23 +1,10 @@
-let date = "10/17/20";
-let RHR = 42;
-let HRV = 45;
-let temp = 98.7;
-let calIn = 2200;
-let calOut = 2100;
-let wgt = 132;
-let fat = 11.7;
-let distance = 10;
-let sleepHrs = 8.0;
-let BP = "118/72"
-/* const inputs = [date, RHR, HRV, temp, calIn, calOut, wgt, fat, distance, sleepHrs, BP] */
+
 //selectors
 const resetBtn = document.querySelector(".resetBtn");
 const submitBtn = document.querySelector(".submitBtn");
 const form = document.querySelector(".form");
 const tBody = document.querySelector(".tBody");
 const tRow = document.querySelector(".tRow");
-const tRowAll = document.querySelectorAll(".tRow");
-
 
 // event listeners
 resetBtn.addEventListener("click", (event) => {
@@ -35,26 +22,68 @@ submitBtn.addEventListener("click", (event) => {
         const addRow = document.createElement("tr");
         addRow.classList.add("tRow")
         
+        //set values from input form
+        date = document.getElementById("date").value;
+        RHR = document.getElementById("RHR").value;
+        HRV = document.getElementById("HRV").value;
+        temp = document.getElementById("temp").value;
+        calIn = document.getElementById("calIn").value;
+        calOut = document.getElementById("calOut").value;
+        wgt = document.getElementById("weight").value;
+        fat = document.getElementById("fat").value;
+        distance = document.getElementById("distance").value;
+        sleepHrs = document.getElementById("sleep").value;
+        BP = document.getElementById("BP").value;
+
         //create table data from inputs array 
         const inputs = [date, RHR, HRV, temp, calIn, calOut, wgt, fat, distance, sleepHrs, BP] 
-        const addData = document.createElement("td");
-        addData.innerText = date;
-        
-        addRow.appendChild(addData); 
+        for (i=0; i<inputs.length; i++){ 
+            const addData = document.createElement("td");
+            addData.innerText = inputs[i];
+            addRow.appendChild(addData);
+        }
+
         
         //append row to table
         tBody.appendChild(addRow);
 
+        //add delete button to row
+        const deleteButton = document.createElement('button');
+        deleteButton.innerText = "X"
+        deleteButton.classList.add("deleteBtn");
+        addRow.appendChild(deleteButton);
+
+        
+
         };
 
-        populateRow("11/17/20");
+        populateRow();
+   
  });
- 
- tRowAll.forEach(item => {
+
+ document.querySelector(".deleteBtn").addEventListener("click", (event) => {
+    event.preventDefault();
+    alert("x clicked");
+    document.querySelector(".tRow").remove();
+ });
+
+/*  document.querySelector(".deleteBtn").addEventListener("click", (event) => {
+    event.preventDefault();
+    alert("x clicked");
+    document.querySelector(".tRow").remove();
+}); */ 
+/* document.querySelector(".deleteBtn").forEach(item => {
+    item.addEventListener("click", (event) => {
+ event.preventDefault();
+ alert("x clicked");
+ document.querySelector(".tRow").remove();
+})}); */
+
+/*  tRowAll.forEach(item => {
     item.addEventListener("dblclick", (event) => {
  event.preventDefault();
  alert("row clicked");
-})});
+})}); */
 
 
 /* function populateRow () {
